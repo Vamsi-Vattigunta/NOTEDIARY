6a53900a67aa42d9a9039342bb7144a3b126d1aa
VAMSI VATTIGUNTA <vamsivattigunta@VAMSIs-MacBook-Air.local>
2024-06-03 22:23:20 +0530

    user profile functionality and deploymentt

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,14 @@ app.use("/api/notes", noteRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
+  console.log(process.env.NODE_ENV);
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
+  console.log("hi2");
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
